bf1cb7c0f0c22d3a4c701c8dc91eb2f83b2f61a0
MooruvyiGoo <53079378+MooruvyiGoo@users.noreply.github.com>
2021-05-01 23:11:23 +0500

    Create sdc-sirtin.js

---
+++ sdc-sirtin.js
@@ -0,0 +1,4 @@
+let img = document.getElementById("mb-logo")
+img.setAttribute("src", "https://memeberry.herokuapp.com/files/logo.svg")
+img.setAttribute("width", "200")
+img.setAttribute("height", "50")
